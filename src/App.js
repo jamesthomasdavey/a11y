2f73b2eb3dcebaf818b1c5233ce134c875f0837d
James Thomas Davey <jamesthomasdavey@james-air.lan>
2021-10-29 15:03:53 -0700

    add resume

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+// package
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { BiLinkExternal, BiMailSend } from "react-icons/bi";
 
+// components
 import DarkModeSwitch from "./components/DarkModeSwitch/DarkModeSwitch";
 import Heading from "./components/Heading/Heading";
 import Accordion from "./components/Accordion/Accordion";
 import ContactForm from "./components/ContactForm/ContactForm";
 import Footer from "./components/Footer/Footer";
 
+// css
 import classes from "./App.module.css";
+
+// other pages
+import Resume from "./components/pages/Resume/Resume";
 
 const App = () => {
   const [darkModeEnabled, setDarkModeEnabled] = useState(false);
@@ -33,129 +40,131 @@ const App = () => {
   });
 
   return (
-    <React.Fragment>
-      <DarkModeSwitch
-        on={darkModeEnabled}
-        toggle={() => {
-          localStorage.setItem("darkModeEnabled", !darkModeEnabled);
-          setDarkModeEnabled(!darkModeEnabled);
-        }}
-      />
-      <main className={classes.main}>
-        <Heading darkModeEnabled={darkModeEnabled} />
-        <p className={classes.paragraph}>
-          I am a <strong>Web Developer</strong> and{" "}
-          <strong>Web Accessibility Specialist</strong> based in Seattle, doing
-          my part to build an inclusive web.
-        </p>
-        <p className={classes.paragraph}>
-          Currently working full-time as an{" "}
-          <strong>Accessibility Engineer</strong> for restaurants’ ordering
-          platforms, I have previously contributed to hundreds of websites and
-          apps in other spaces such as healthcare, entertainment, retail, and
-          software. For more about my skills and qualifications,{" "}
-          <a
-            href="https://jamesthomasdavey.com/resume"
-            target="_blank"
-            rel="noreferrer"
-          >
-            check out my resume{" "}
-            <BiLinkExternal
-              aria-label="Opens in new tab"
-              role="img"
-              focusable="false"
-            />
-          </a>
-          .
-        </p>
-        <section>
-          <h2
-            className={[
-              classes.h2,
-              darkModeEnabled ? classes.darkMode : "",
-            ].join(" ")}
-          >
-            I do web accessibility.
-          </h2>
-          <p className={classes.paragraph}>
-            I love the challenges of deconstructing unfamiliar or complex
-            interfaces into simplified operable parts, finding ways to linearize
-            two-dimentional layouts, and allowing assistive technology to read
-            and operate everything. Few things satisfy me more than using a
-            keyboard to navigate a robustly-built website with the screen reader
-            on.
-          </p>
-          <p className={classes.paragraph}>
-            Accessibility tends to show up late; whether in peoples' engineering
-            careers because the topic was either ignored or only briefly
-            mentioned in school or bootcamps, or in projects because users with
-            disabilities were not considered in the design and development
-            stages. Accessibility can therefore appear to be a daunting task,
-            but my hope is to ease this process as much as possible and involve
-            accessibility in the earliest stages. It's never too late to start
-            accessibility, but it's never too early either.
-          </p>
-        </section>
-        <section>
-          <h2
-            className={[
-              classes.h2,
-              darkModeEnabled ? classes.darkMode : "",
-            ].join(" ")}
-          >
-            Things I can do.
-          </h2>
-          <Accordion darkModeEnabled={darkModeEnabled} />
-        </section>
-        <section>
-          <h2
-            id="say-hello-heading"
-            className={[
-              classes.h2,
-              darkModeEnabled ? classes.darkMode : "",
-            ].join(" ")}
-          >
-            <span
-              className={[
-                classes.emoji,
-                darkModeEnabled ? classes.darkMode : "",
-              ].join(" ")}
-              aria-hidden="true"
-            >
-              👋
-            </span>{" "}
-            Say hello!
-          </h2>
-          <p className={classes.paragraph}>
-            Email me at{" "}
-            <a href="mailto:james.davey@fastmail.com">
-              james.davey@fastmail.com{" "}
-              <BiMailSend
-                aria-label="Composes a new email"
-                role="img"
-                focusable="false"
-              />
-            </a>
-            ,{" "}
-            <a
-              href="https://www.linkedin.com/in/jamesthomasdavey/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              visit my LinkedIn{" "}
-              <BiLinkExternal
-                aria-label="Opens in new tab"
-                role="img"
-                focusable="false"
-              />
-            </a>
-            , or send a message using the form below:
-          </p>
-          <ContactForm darkModeEnabled={darkModeEnabled} />
-        </section>
-      </main>
-      <Footer darkModeEnabled={darkModeEnabled} />
-    </React.Fragment>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <DarkModeSwitch
+            on={darkModeEnabled}
+            toggle={() => {
+              localStorage.setItem("darkModeEnabled", !darkModeEnabled);
+              setDarkModeEnabled(!darkModeEnabled);
+            }}
+          />
+          <main className={classes.main}>
+            <Heading darkModeEnabled={darkModeEnabled} />
+            <p className={classes.paragraph}>
+              I am a <strong>Web Developer</strong> and{" "}
+              <strong>Web Accessibility Specialist</strong> based in Seattle,
+              doing my part to build an inclusive web.
+            </p>
+            <p className={classes.paragraph}>
+              Currently working full-time as an{" "}
+              <strong>Accessibility Engineer</strong> for restaurants’ ordering
+              platforms, I have previously contributed to hundreds of websites
+              and apps in other spaces such as healthcare, entertainment,
+              retail, and software. For more about my skills and qualifications,{" "}
+              <Link to="/resume" target="_blank" rel="noreferrer">
+                check out my resume{" "}
+                <BiLinkExternal
+                  aria-label="Opens in new tab"
+                  role="img"
+                  focusable="false"
+                />
+              </Link>
+              .
+            </p>
+            <section>
+              <h2
+                className={[
+                  classes.h2,
+                  darkModeEnabled ? classes.darkMode : "",
+                ].join(" ")}
+              >
+                I do web accessibility.
+              </h2>
+              <p className={classes.paragraph}>
+                I love the challenges of deconstructing unfamiliar or complex
+                interfaces into simplified operable parts, finding ways to
+                linearize two-dimentional layouts, and allowing assistive
+                technology to read and operate everything. Few things satisfy me
+                more than using a keyboard to navigate a robustly-built website
+                with the screen reader on.
+              </p>
+              <p className={classes.paragraph}>
+                Accessibility tends to show up late; whether in peoples'
+                engineering careers because the topic was either ignored or only
+                briefly mentioned in school or bootcamps, or in projects because
+                users with disabilities were not considered in the design and
+                development stages. Accessibility can therefore appear to be a
+                daunting task, but my hope is to ease this process as much as
+                possible and involve accessibility in the earliest stages. It's
+                never too late to start accessibility, but it's never too early
+                either.
+              </p>
+            </section>
+            <section>
+              <h2
+                className={[
+                  classes.h2,
+                  darkModeEnabled ? classes.darkMode : "",
+                ].join(" ")}
+              >
+                Things I can do.
+              </h2>
+              <Accordion darkModeEnabled={darkModeEnabled} />
+            </section>
+            <section>
+              <h2
+                id="say-hello-heading"
+                className={[
+                  classes.h2,
+                  darkModeEnabled ? classes.darkMode : "",
+                ].join(" ")}
+              >
+                <span
+                  className={[
+                    classes.emoji,
+                    darkModeEnabled ? classes.darkMode : "",
+                  ].join(" ")}
+                  aria-hidden="true"
+                >
+                  👋
+                </span>{" "}
+                Say hello!
+              </h2>
+              <p className={classes.paragraph}>
+                Email me at{" "}
+                <a href="mailto:james.davey@fastmail.com">
+                  james.davey@fastmail.com{" "}
+                  <BiMailSend
+                    aria-label="Composes a new email"
+                    role="img"
+                    focusable="false"
+                  />
+                </a>
+                ,{" "}
+                <a
+                  href="https://www.linkedin.com/in/jamesthomasdavey/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  visit my LinkedIn{" "}
+                  <BiLinkExternal
+                    aria-label="Opens in new tab"
+                    role="img"
+                    focusable="false"
+                  />
+                </a>
+                , or send a message using the form below:
+              </p>
+              <ContactForm darkModeEnabled={darkModeEnabled} />
+            </section>
+          </main>
+          <Footer darkModeEnabled={darkModeEnabled} />
+        </Route>
+        <Route exact path="/resume" component={Resume} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/components/pages/Resume/Resume.js
@@ -0,0 +1,104 @@
+import React from "react";
+
+import classes from "./Resume.module.css";
+
+import downloadicon from "./img/downloadicon.svg";
+import resumePdf from "./pdf/James Thomas Davey - Resume.pdf";
+
+import resumeData from "./resumeData";
+
+const ResumePrint = () => {
+  document.title = "Resume | James Thomas Davey";
+  document
+    .querySelector("html")
+    .setAttribute("style", "background-color: #8fd2af;");
+  return (
+    <div className={classes.wrapper}>
+      <main className={classes.wrapper__inner}>
+        <h1 className={classes.nameHeading}>{resumeData.name}</h1>
+        <div className={classes.downloadLinkWrapper}>
+          <a
+            className={classes.downloadLink}
+            href={resumePdf}
+            download="James Thomas Davey - Resume"
+          >
+            <img alt="Download Resume" src={downloadicon} />
+          </a>
+        </div>
+        <section>
+          {/* <h2 className={classes.titleHeading}>{resumeData.jobTitle}</h2> */}
+          <ul className={classes.subtitleList}>
+            <li>{resumeData.location}</li>
+            <li>
+              <a href={`mailto:${resumeData.email}`}>{resumeData.email}</a>
+            </li>
+            <li>{resumeData.phoneNumber}</li>
+          </ul>
+          <p className={classes.summary}>{resumeData.summary}</p>
+        </section>
+        <section aria-labelledby="work-experience-heading">
+          <h2 id="work-experience-heading" className={classes.subHeading}>
+            Work Experience
+          </h2>
+          {resumeData.workExperiences.map((workExperience) => {
+            return (
+              <section>
+                <h3 className={classes.dateRoleWrapper}>
+                  <div className={classes.dateHeading}>
+                    {workExperience.dates}
+                  </div>
+                  <div className={classes.roleHeading}>
+                    {workExperience.company}, {workExperience.title}
+                  </div>
+                </h3>
+                <div className={classes.expContent}>
+                  {workExperience.summary && <p>{workExperience.summary}</p>}
+                  <ul>
+                    {workExperience.responsibilities.map((responsibility) => {
+                      return <li>{responsibility}</li>;
+                    })}
+                  </ul>
+                </div>
+              </section>
+            );
+          })}
+        </section>
+        <section
+          aria-labelledby="education-heading"
+          className={classes.educationSection}
+        >
+          <h2
+            id="education-heading"
+            className={[classes.subHeading, classes.educationHeading].join(" ")}
+          >
+            Education
+          </h2>
+          <section>
+            <h3
+              className={[classes.roleHeading, classes.schoolHeading].join(" ")}
+            >
+              {resumeData.education.school}
+            </h3>
+            <div className={classes.educationContent}>
+              <p>
+                <span>{resumeData.education.degree}</span>
+              </p>
+            </div>
+          </section>
+        </section>
+        <section aria-labelledby="technical-skills-heading">
+          <h2 id="technical-skills-heading" className={classes.subHeading}>
+            Technical Skills
+          </h2>
+          <ul className={classes.skillsList}>
+            {resumeData.technicalSkills.map((technicalSkill) => {
+              return <li>{technicalSkill}</li>;
+            })}
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default ResumePrint;
