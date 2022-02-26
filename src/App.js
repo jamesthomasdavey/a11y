290efb9f8b07ab37c7daf2397785b7c3411902d9
James Thomas Davey <james.t.davey@gmail.com>
2022-02-25 20:33:31 -0800

    add the prefix to my job title

--- src/App.js
+++ src/App.js
@@ -1,82 +1,82 @@
 // package
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { BiLinkExternal, BiMailSend } from "react-icons/bi";
-import { FocusRing } from "@react-aria/focus";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BiLinkExternal, BiMailSend } from 'react-icons/bi';
+import { FocusRing } from '@react-aria/focus';
 
 // components
-import DarkModeSwitch from "./components/DarkModeSwitch/DarkModeSwitch";
-import Heading from "./components/Heading/Heading";
-import Accordion from "./components/Accordion/Accordion";
-import ContactForm from "./components/ContactForm/ContactForm";
-import Footer from "./components/Footer/Footer";
+import DarkModeSwitch from './components/DarkModeSwitch/DarkModeSwitch';
+import Heading from './components/Heading/Heading';
+import Accordion from './components/Accordion/Accordion';
+import ContactForm from './components/ContactForm/ContactForm';
+import Footer from './components/Footer/Footer';
 
 // css
-import classes from "./App.module.css";
+import classes from './App.module.css';
 
 // other pages
-import Resume from "./components/pages/Resume/Resume";
+import Resume from './components/pages/Resume/Resume';
 
 const App = () => {
   const [darkModeEnabled, setDarkModeEnabled] = useState(false);
 
   useEffect(() => {
     if (
-      localStorage.darkModeEnabled === "true" ||
-      localStorage.darkModeEnabled === "false"
+      localStorage.darkModeEnabled === 'true' ||
+      localStorage.darkModeEnabled === 'false'
     ) {
       setDarkModeEnabled(
-        localStorage.darkModeEnabled === "true" ? true : false
+        localStorage.darkModeEnabled === 'true' ? true : false
       );
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       setDarkModeEnabled(true);
     }
   }, []);
 
   useEffect(() => {
     darkModeEnabled
-      ? document.querySelector("body").classList.add("darkMode")
-      : document.querySelector("body").classList.remove("darkMode");
+      ? document.querySelector('body').classList.add('darkMode')
+      : document.querySelector('body').classList.remove('darkMode');
   });
 
   return (
     <Router>
       <Switch>
-        <Route exact path="/resume" component={Resume} />
-        <Route path="/">
+        <Route exact path='/resume' component={Resume} />
+        <Route path='/'>
           <DarkModeSwitch
             on={darkModeEnabled}
             toggle={() => {
-              localStorage.setItem("darkModeEnabled", !darkModeEnabled);
+              localStorage.setItem('darkModeEnabled', !darkModeEnabled);
               setDarkModeEnabled(!darkModeEnabled);
             }}
           />
           <main
             className={[
               classes.main,
-              darkModeEnabled ? classes.darkMode : "",
-            ].join(" ")}
+              darkModeEnabled ? classes.darkMode : '',
+            ].join(' ')}
           >
             <Heading darkModeEnabled={darkModeEnabled} />
             <p className={classes.paragraph}>
-              I am a <strong>Web Developer</strong> and{" "}
+              I am a <strong>Web Developer</strong> and{' '}
               <strong>Accessibility Specialist</strong> based in Seattle,
               Washington, doing my part to build an inclusive web.
             </p>
             <p className={classes.paragraph}>
-              Currently working full-time as an{" "}
-              <strong>Accessibility Engineer</strong> developing first-party
-              restaurant ordering platforms, I have previously contributed to
-              hundreds of websites and apps in other spaces such as healthcare,
-              entertainment, retail, and software. For more about my skills and
-              qualifications,{" "}
-              <FocusRing focusRingClass="focus-ring">
-                <Link to="/resume" target="_blank" rel="noreferrer">
-                  check out my resume{" "}
+              Currently working full-time as a{' '}
+              <strong>Senior Accessibility Engineer</strong> developing
+              first-party restaurant ordering platforms, I have previously
+              contributed to hundreds of websites and apps in other spaces such
+              as healthcare, entertainment, retail, and software. For more about
+              my skills and qualifications,{' '}
+              <FocusRing focusRingClass='focus-ring'>
+                <Link to='/resume' target='_blank' rel='noreferrer'>
+                  check out my resume{' '}
                   <BiLinkExternal
-                    aria-label="Opens in new tab"
-                    role="img"
-                    focusable="false"
+                    aria-label='Opens in new tab'
+                    role='img'
+                    focusable='false'
                   />
                 </Link>
               </FocusRing>
@@ -86,8 +86,8 @@ const App = () => {
               <h2
                 className={[
                   classes.h2,
-                  darkModeEnabled ? classes.darkMode : "",
-                ].join(" ")}
+                  darkModeEnabled ? classes.darkMode : '',
+                ].join(' ')}
               >
                 I do web accessibility.
               </h2>
@@ -114,8 +114,8 @@ const App = () => {
               <h2
                 className={[
                   classes.h2,
-                  darkModeEnabled ? classes.darkMode : "",
-                ].join(" ")}
+                  darkModeEnabled ? classes.darkMode : '',
+                ].join(' ')}
               >
                 Things I can do.
               </h2>
@@ -123,47 +123,47 @@ const App = () => {
             </section>
             <section>
               <h2
-                id="say-hello-heading"
+                id='say-hello-heading'
                 className={[
                   classes.h2,
-                  darkModeEnabled ? classes.darkMode : "",
-                ].join(" ")}
+                  darkModeEnabled ? classes.darkMode : '',
+                ].join(' ')}
               >
                 <span
                   className={[
                     classes.emoji,
-                    darkModeEnabled ? classes.darkMode : "",
-                  ].join(" ")}
-                  aria-hidden="true"
+                    darkModeEnabled ? classes.darkMode : '',
+                  ].join(' ')}
+                  aria-hidden='true'
                 >
                   👋
-                </span>{" "}
+                </span>{' '}
                 Say hello!
               </h2>
               <p className={classes.paragraph}>
-                Email me at{" "}
-                <FocusRing focusRingClass="focus-ring">
-                  <a href="mailto:james.davey@fastmail.com">
-                    james.davey@fastmail.com{" "}
+                Email me at{' '}
+                <FocusRing focusRingClass='focus-ring'>
+                  <a href='mailto:james.davey@fastmail.com'>
+                    james.davey@fastmail.com{' '}
                     <BiMailSend
-                      aria-label="Composes a new email"
-                      role="img"
-                      focusable="false"
+                      aria-label='Composes a new email'
+                      role='img'
+                      focusable='false'
                     />
                   </a>
                 </FocusRing>
-                ,{" "}
-                <FocusRing focusRingClass="focus-ring">
+                ,{' '}
+                <FocusRing focusRingClass='focus-ring'>
                   <a
-                    href="https://www.linkedin.com/in/jamesthomasdavey/"
-                    target="_blank"
-                    rel="noreferrer"
+                    href='https://www.linkedin.com/in/jamesthomasdavey/'
+                    target='_blank'
+                    rel='noreferrer'
                   >
-                    visit my LinkedIn{" "}
+                    visit my LinkedIn{' '}
                     <BiLinkExternal
-                      aria-label="Opens in new tab"
-                      role="img"
-                      focusable="false"
+                      aria-label='Opens in new tab'
+                      role='img'
+                      focusable='false'
                     />
                   </a>
                 </FocusRing>
