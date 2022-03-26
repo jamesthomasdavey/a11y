8cac60ecb41eacbc9da9dd4cf608540cf1149c84
James Davey <james.davey@fastmail.com>
2022-03-25 21:56:28 -0700

    rewrite accordion paragraphs, revert chevron animation

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -7,27 +7,32 @@ const Accordion = ({ darkModeEnabled, hasLoaded }) => {
     {
       heading: "Testing",
       paragraph:
-        "Using automated and manual testing tools to audit your website, I can estimate overall risk, identify what is and isn't accessible, tie all issues to relevant WCAG success criteria, and most importantly, provide instructions on fixing those issues.",
+        "Using automated and manual accessibility testing tools to audit your website, I can estimate overall risk, identify what is and isn't accessible, tie all issues to relevant WCAG success criteria, and most importantly, provide instructions on fixing those issues.",
     },
     {
       heading: "Design Reviews",
       paragraph:
-        "Get ahead by pushing accessibility to the left. I can analyze wireframes and tell you the problem areas, such as insufficient contrast, spacing, color dependencies, and potential layout issues from the perspective of users with disabilities.",
+        "Accessible development starts with accessible design. I can analyze wireframes and tell you the problem areas, such as insufficient contrast, spacing, color dependencies, and layout issues that may affect users with disabilities.",
     },
     // {
     //   heading: "Planning",
     //   paragraph:
     //     "An accessible component library is paramount for a consistent user experience. Even a perfectly semantic page structure can quickly be nullified if users cannot understand or operate the components within it. By focusing on accessible building blocks of all sizes, I can help you build a seamless experience for all users.",
     // },
+    // {
+    //   heading: "Planning",
+    //   paragraph:
+    //     "Accessibility tends to show up late; the topic is often ignored during tech education and is often an afterthought in web and app projects. Implementing accessibility can therefore appear to be a daunting task, but my hope is to ease this process as much as possible and eventually rethink how we approach design and development by including accessibility in the earliest possible stages.",
+    // },
     {
-      heading: "Planning",
+      heading: "Handoff",
       paragraph:
-        "Accessibility tends to show up late; the topic is often ignored during tech education and is often an afterthought in web and app projects. Implementing accessibility can therefore appear to be a daunting task, but my hope is to ease this process as much as possible and eventually rethink how we approach design and development by including accessibility in the earliest possible stages.",
+        "Even the most accessible designs can be a wasted effort if developers aren't aware of factors such as focus order, reading order, proper semantic containers and controls, and various attributes that help assistive technology users. I can mark up your designs with comments conveying exactly that, removing any guesswork.",
     },
     {
-      heading: "Handoff Annotations",
+      heading: "Development",
       paragraph:
-        "When handing designs off to a development team, it's best not to make them guess which semantics and attributes they need for properly exposing information for assistive technology users. I can add comments to your wireframes conveying exactly that, removing any guesswork.",
+        "A robust component library is essential for any modern web application and is far-reaching in maintaining accessibility. Using ReactJS, I develop reusable components to uphold and enhance the accessibility of their designs.",
     },
     {
       heading: "Guidance",
